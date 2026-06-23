25c5d7b601647012d680f02bf601d5d1e15ce3b6
Lorgio Teodovich <lteodovich@herrera.unt.edu.ar>
2026-06-23 00:09:51 -0300

    Ya compila, y no pasa la prueba
    Da el error:  At line (17): "Element 0 Expected 176 Was 0"

---
+++ src/reloj.c
@@ -0,0 +1,54 @@
+/*********************************************************************************************************************
+Copyright 2025-2026, Electrónica IV
+Facultad de Ciencias Exactas y Tecnologia
+Universidad Nacional de Tucuman
+https://facetvirtual.facet.unt.edu.ar/course/view.php?id=165
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/** @file reloj.c
+ ** @brief
+ **/
+
+/* === Headers files inclusions ==================================================================================== */
+#include "reloj.h" /**< Librería del reloj*/
+
+/* === Macros definitions ====================================================================== */
+
+/* === Private data type declarations ========================================================== */
+
+/* === Private function declarations =========================================================== */
+
+/* === Private variable definitions ============================================================ */
+
+/* === Public variable definition  ============================================================= */
+
+/* === Private function definitions ============================================================ */
+
+/* === Public function implementation ========================================================== */
+
+reloj_t RelojCreate(uint16_t tiks_per_second, void * alarm_handler) {
+}
+
+bool RelojGetCurrentTime(reloj_t reloj, hora_t hora_actual) {
+}
+
+/* === End of documentation ==================================================================== */
